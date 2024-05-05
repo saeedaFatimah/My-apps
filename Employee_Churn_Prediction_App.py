6173d471f317251fb93520b3bf2df7e467566306
saeedaFatimah <78494691+saeedaFatimah@users.noreply.github.com>
2024-05-06 03:34:00 +0500

    Employee_Churn_Prediction_App.py

--- Employee_Churn_Prediction_App.py
+++ Employee_Churn_Prediction_App.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 import dill
-from sklearn.impute import SimpleImputer
 
 # Load the pretrained model
 with open('pipeline.pkl', 'rb') as file:
@@ -12,12 +11,10 @@ my_feature_dict = load('my_feature_dict.pkl')
 
 # Function to predict churn
 def predict_churn(data):
-    # Impute missing values
-    imputer = SimpleImputer(strategy='mean')
-    data_imputed = imputer.fit_transform(data)
-    
-    # Make predictions
-    prediction = model.predict(data_imputed)
+    # Convert categorical columns to numeric using one-hot encoding
+    data_encoded = pd.get_dummies(data, drop_first=True)
+    # Predict churn
+    prediction = model.predict(data_encoded)
     return prediction
 
 st.title('Employee Churn Prediction App')
@@ -26,30 +23,25 @@ st.subheader('Based on Employee Dataset')
 # Display categorical features
 st.subheader('Categorical Features')
 categorical_input = my_feature_dict.get('CATEGORICAL')
-categorical_input_vals={}
-for i, col in enumerate(categorical_input.get('Column Name').values()):
+categorical_input_vals = {}
+for i, col in enumerate(categorical_input.get('Column Name')):
     categorical_input_vals[col] = st.selectbox(col, categorical_input.get('Members')[i])
-
-# Load numerical features
-numerical_input = my_feature_dict.get('NUMERICAL')
 
 # Display numerical features
 st.subheader('Numerical Features')
 numerical_input = my_feature_dict.get('NUMERICAL')
-numerical_input_vals={}
-for col in  numerical_input.get('Column Name'):
+numerical_input_vals = {}
+for col in numerical_input.get('Column Name'):
     numerical_input_vals[col] = st.number_input(col)
 
 # Combine numerical and categorical input dicts
-input_data = dict(list(categorical_input_vals.items()) + list(numerical_input_vals.items()))
-
-input_data= pd.DataFrame.from_dict(input_data,orient='index').T
+input_data = pd.DataFrame.from_dict({**categorical_input_vals, **numerical_input_vals}, orient='index').T
 
 # Churn Prediction
 if st.button('Predict'):
     prediction = predict_churn(input_data)[0]
-    translation_dict = {"Yes": "Expected", "No": "Not Expected"}
+    translation_dict = {1: "Expected", 0: "Not Expected"}
     prediction_translate = translation_dict.get(prediction)
     st.write(f'The Prediction is **{prediction}**, Hence customer is **{prediction_translate}** to churn.')
-    
+
 st.subheader('Created by Saeeda Fatima')
