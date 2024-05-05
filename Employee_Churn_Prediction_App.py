ec7a08b1487aee7e66492915ebc2c11145c43d73
saeedaFatimah <78494691+saeedaFatimah@users.noreply.github.com>
2024-05-06 03:43:22 +0500

    Employee_Churn_Prediction_App.py

--- Employee_Churn_Prediction_App.py
+++ Employee_Churn_Prediction_App.py
@@ -11,10 +11,7 @@ my_feature_dict = load('my_feature_dict.pkl')
 
 # Function to predict churn
 def predict_churn(data):
-    # Convert categorical columns to numeric using one-hot encoding
-    data_encoded = pd.get_dummies(data, drop_first=True)
-    # Predict churn
-    prediction = model.predict(data_encoded)
+    prediction = model.predict(data)
     return prediction
 
 st.title('Employee Churn Prediction App')
@@ -23,25 +20,30 @@ st.subheader('Based on Employee Dataset')
 # Display categorical features
 st.subheader('Categorical Features')
 categorical_input = my_feature_dict.get('CATEGORICAL')
-categorical_input_vals = {}
+categorical_input_vals={}
 for i, col in enumerate(categorical_input.get('Column Name')):
-    categorical_input_vals[col] = st.selectbox(col, categorical_input.get('Members')[i])
+    members = categorical_input.get('Members')[i]
+    categorical_input_vals[col] = st.selectbox(str(col), members)
+
+# Load numerical features
+numerical_input = my_feature_dict.get('NUMERICAL')
 
 # Display numerical features
 st.subheader('Numerical Features')
-numerical_input = my_feature_dict.get('NUMERICAL')
-numerical_input_vals = {}
+numerical_input_vals={}
 for col in numerical_input.get('Column Name'):
     numerical_input_vals[col] = st.number_input(col)
 
 # Combine numerical and categorical input dicts
-input_data = pd.DataFrame.from_dict({**categorical_input_vals, **numerical_input_vals}, orient='index').T
+input_data = dict(list(categorical_input_vals.items()) + list(numerical_input_vals.items()))
+
+input_data= pd.DataFrame.from_dict(input_data,orient='index').T
 
 # Churn Prediction
 if st.button('Predict'):
     prediction = predict_churn(input_data)[0]
-    translation_dict = {1: "Expected", 0: "Not Expected"}
+    translation_dict = {"Yes": "Expected", "No": "Not Expected"}
     prediction_translate = translation_dict.get(prediction)
     st.write(f'The Prediction is **{prediction}**, Hence customer is **{prediction_translate}** to churn.')
-
+    
 st.subheader('Created by Saeeda Fatima')
